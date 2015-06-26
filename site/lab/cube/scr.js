c8afc9d9ddec5025bb3f675ce190965c7f12ef6c
Dead Seagull <di.ed.seagull@gmail.com>
2015-06-26 13:55:03 +0300

    menu

--- site/lab/cube/scr.js
+++ site/lab/cube/scr.js
@@ -1,25 +1,24 @@
 document.onmousemove = mousecoord;
 
-var param = {
-	amp: 4,
-	persp: 750
-}
+var x, y, xPer, yPer, rotX, rotY, persp,
+	menu_el = document.querySelectorAll(".block"),
+	param   = { amp: 2, persp: 748 };
 
 function mousecoord(e)
 {
 
-	var _x = e.clientX - window.innerWidth/2,
-		_y = e.clientY - window.innerHeight/2;
+	x = e.clientX - 200,
+	y = e.clientY - window.innerHeight/2;
 
-	var xPer = Math.floor( (_x*100)/window.innerWidth  ),
-		yPer = Math.floor( (_y*100)/window.innerHeight );
+	xPer = Math.floor( ( x * 100 ) / window.innerWidth / 2  ),
+	yPer = Math.floor( ( y * 100 ) / window.innerHeight / 2 );
 
-	var rotY = "rotateY(" + xPer/param.amp + "deg)",
-		rotX = "rotateX(" + yPer/-param.amp + "deg)",
-		moveX = "translateX(" + xPer*(param.amp+2) + "px)",
-		moveY = "translateY(" + yPer*param.amp + "px)";
+	persp = "perspective(" + param.persp    + ")"   ,
+	rotY  = "rotateY("     + xPer/param.amp + "deg)",
+	rotX  = "rotateX("     + yPer/param.amp + "deg)";
 
-	document.querySelector(".block")
-	.style
-	.webkitTransform = "perspective("+param.persp+")" + rotY + rotX + moveX + moveY;
+	for (var i=0; menu_el.length; i++)
+	{
+		menu_el[i].style.webkitTransform = persp + rotY + rotX;
+	}
 }
