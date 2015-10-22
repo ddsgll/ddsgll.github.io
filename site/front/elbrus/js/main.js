ad450feca296755bb3a872fe810b714499e181d4
Dead Seagull <di.ed.seagull@gmail.com>
2015-10-22 17:00:56 +0300

    Preloader + Cable

--- site/front/elbrus/js/main.js
+++ site/front/elbrus/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
 
 
-	// Mobile menu actions
+	/*
+	================================================================
+		| Main general |
+	================================================================
+	*/
+	if ( $(window).width() > 1200 ) {
+		initOnepageScroll();
+	}
+
+	animateMainMenuLinks();
+
+
+
+
+
+
+
+
+
+
+	/*
+	================================================================
+		| Mobile menu |
+	================================================================
+	*/
+
+
 	var mob_open  = $("#open_menu"),
 		mob_close = $("#close_menu");
 
@@ -21,7 +47,20 @@ $(document).ready(function() {
 
 
 
-	// Change images to bg
+
+
+
+
+
+
+
+	/*
+	================================================================
+		| Images to bg |
+	================================================================
+	*/
+
+
 	$(".slides .slide").each(function() {
 		var cur_img = $(this).children(".image").find("img"),
 			cur_src = cur_img.attr("src");
@@ -33,34 +72,56 @@ $(document).ready(function() {
 
 
 
-	if ( $(window).width() > 1200 ) {
-		initOnepageScroll();
-	}
-
-
-	animateMainMenuLinks();
 
 
 
-	// Main page slider settings
-	$(".main_page_slider .slides").flickity({
+
+
+
+
+	/*
+	================================================================
+		| Main slider |
+	================================================================
+	*/
+
+	var main_slider = $(".main_page_slider .slides"),
+		main_prev = $("#main-slide-left"),
+		main_next = $("#main-slide-right");
+
+	main_slider.flickity({
 		cellSelector    : '.slide',
 		prevNextButtons : false,
 		wrapAround      : false,
 		pageDots        : true
 	});
 
-	$("#main-slide-left").on('click', function() {
-		$(".main_page_slider .slides").flickity("previous");
+
+	// Next-Prev buttons
+	main_prev.on('click', function() {
+		main_slider.flickity("previous");
 	});
 
-	$("#main-slide-right").on('click', function() {
-		$(".main_page_slider .slides").flickity("next");
+	main_next.on('click', function() {
+		main_slider.flickity("next");
 	});
 
 
 
-	// 3D button behavior
+
+
+
+
+
+
+
+	/*
+	================================================================
+		| 3D button |
+	================================================================
+	*/
+
+
 	var btn = $(".button3d");
 
 	btn.on('mousemove', function(e) {
@@ -80,20 +141,115 @@ $(document).ready(function() {
 
 
 
-	// Webcam
-	var webc_item = $(".webcam_item");
 
-	webc_item.on('click', function() {
-		$(".webcam_splash").addClass("active");
+
+
+
+
+
+
+	/*
+	================================================================
+		| Live sections toggle |
+	================================================================
+	*/
+
+	var tabs = $(".live_tabs .live_tab_button"),
+		mob_tabs = $(".mobile_tabs .tab"),
+		live_state = "webcam",
+		live_section = $(".live_section"),
+		sections = $(".livesect");
+
+	sections.fadeOut(0);
+
+	reload_live_section( live_state );
+
+
+	tabs.on('click', function() {
+		tabs.removeClass("active");
+		$(this).addClass("active");
+		live_state = $(this).data('state');
+		sections.fadeOut(0);
+		reload_live_section( live_state );
 	});
 
-	$(".webcam_splash .videoblock").find(".close").on('click', function() {
-		$(".webcam_splash").removeClass("active");
-	})
+	mob_tabs.on('click', function() {
+		mob_tabs.removeClass("active");
+		$(this).addClass("active");
+		live_state = $(this).data('state');
+		sections.fadeOut(0);
+		reload_live_section( live_state );
+	});
 
 
 
-	// Main info style fixes
+	function reload_live_section(st) {
+
+		sections.each(function() {
+			var t_state = $(this).data('stcheck');
+
+			if ( st === t_state ) {
+				
+				$(this).fadeIn(500);
+
+			}
+			else {
+				$(this).fadeOut(500);
+			}
+		});
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	================================================================
+		| Webcam |
+	================================================================
+	*/
+
+
+	var webc_item = $(".webcam_item"),
+		webc_splash = $(".webcam_splash"),
+		close = $(".webcam_splash .videoblock").find(".close");
+
+	webc_item.on('click', function() {
+		webc_splash.addClass("active");
+	});
+
+	close.on('click', function() {
+		webc_splash.removeClass("active");
+	});
+
+
+
+
+
+
+
+
+
+	
+	/*
+	================================================================
+		| Style fixes |
+	================================================================
+	*/
+
+
+	// Main info
 	var info_block = $(".info_block .info");
 
 	info_block.each(function() {
@@ -116,8 +272,22 @@ $(document).ready(function() {
 
 
 
-	// Questions page
-	var q_photo = $(".answer .photo");
+
+
+
+
+
+
+	/*
+	================================================================
+		| Questins page |
+	================================================================
+	*/
+
+
+	var q_photo = $(".answer .photo"),
+		answer = $(".answer"),
+		question = $(".question");
 
 	q_photo.each(function() {
 
@@ -127,9 +297,9 @@ $(document).ready(function() {
 
 	});
 
-	$(".answer").slideUp(0);
+	answer.slideUp(0);
 
-	$(".question").on('click', function(e) {
+	question.on('click', function(e) {
 
 		$(this).next(".answer").stop(1,1).slideToggle(500).toggleClass("active");
 
@@ -138,19 +308,37 @@ $(document).ready(function() {
 
 
 
-	// Feedback slider
-	$("#feedback_slider").flickity({
+
+
+
+
+
+
+	/*
+	================================================================
+		| Feedback |
+	================================================================
+	*/
+
+
+	var fb_slider = $("#feedback_slider"),
+		fb_prev = $(".feedback_slider .button.prev"),
+		fb_next = $(".feedback_slider .button.next");
+
+
+	fb_slider.flickity({
 		cellSelector: '.slide',
 		prevNextButtons: false,
 		pageDots: false,
 		wrapAround: false
 	});
 
-	$(".slider .button.prev").on('click', function() {
-		$("#feedback_slider").flickity('previous')
+	fb_prev.on('click', function() {
+		fb_slider.flickity('previous');
 	});
-	$(".slider .button.next").on('click', function() {
-		$("#feedback_slider").flickity('next')
+
+	fb_next.on('click', function() {
+		fb_slider.flickity('next');
 	});
 
 
@@ -158,7 +346,22 @@ $(document).ready(function() {
 
 
 
+
+
+
+
+
+
+
+/*
+================================================================
+	| OnePage Scroll |
+================================================================
+*/
+
+
 function initOnepageScroll() {
+
 	// One page scrolling settings
 	var slider_section = $(".main_sections");
 
@@ -195,7 +398,6 @@ function initOnepageScroll() {
 						break
 
 					case 3:
-						animateNewsSection();
 						$(".webcam_splash").removeClass("active");
 						$(".main_menu").addClass("floated");
 						break
@@ -228,6 +430,8 @@ function initOnepageScroll() {
 		    }
 		});
 
+
+		// Centrifying pageDots
 		var onepage_offset = -1 * $(".onepage-pagination").height() / 2;
 
 		$(".onepage-pagination")
@@ -238,9 +442,43 @@ function initOnepageScroll() {
 
 
 
+
+
+
+
+
+
+
+/*
+================================================================
+	| Preloader |
+================================================================
+*/
+
+
 $(window).load(function() {
 
-	
+	var preloader = $(".preloader");
+
+	window.setTimeout(function() {
+		
+
+		preloader.find(".logo").addClass("stop");
+
+		TweenMax.to(preloader.find(".logo"), 1.2,
+			{
+				scale: [0,0],
+				opacity: 0,
+				ease: Back.easeInOut.config(3),
+				onComplete: cleanSplash
+			});
+
+		function cleanSplash() {
+			preloader.fadeOut(500);
+		}
+	}, 1500);
+
+
 
 });
 
@@ -253,10 +491,84 @@ $(window).load(function() {
 
 
 
-// GSAP Animations
+/*
+================================================================
+	| Main page map |
+================================================================
+*/
+
+
+// Hide map on small screens
+if ( $(".mailmap .map").length )
+{
+
+	if ( $(window).width() > 720 ) {
+	    google.maps.event.addDomListener(window, 'load', initializeMainMap);
+	}
+	else
+	{
+		$(".mailmap .map").remove();
+	}
+
+}
+
+
+// Map initialisation
+function initializeMainMap() {
+
+	var mapOptions, map, myLatLng, imageMarker, marker;
+
+	mapOptions = {
+		zoom: 11,
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: new google.maps.LatLng(43.2631521,42.5343502)
+	}
+
+	map = new google.maps.Map(document.getElementById('main_map'), mapOptions);
+
+	myLatLng = new google.maps.LatLng(43.2631521,42.5343502);
+
+	imageMarker = {
+		url: 'img/icons/map_baloon.png',
+		size: new google.maps.Size(309, 76),
+		origin: new google.maps.Point(0, 0),
+		anchor: new google.maps.Point(309, 76),
+		scaledSize: new google.maps.Size(309, 76)
+	};
+
+	marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: "Поляна Азау",
+		icon: imageMarker
+	});
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+================================================================
+	| GSAP Animations |
+================================================================
+*/
+
+
+// Animating main menu links
 function animateMainMenuLinks() {
 
-	// Animating main menu links
 	var main_menu_links = $(".main_menu .links.normal").find("ul.main > li");
 
 	TweenMax.staggerFromTo( main_menu_links, 2,
@@ -273,23 +585,9 @@ function animateMainMenuLinks() {
 
 }
 
-function animateNewsSection() {
-	// Animating one page scroll pagination
-	// var nws_items = $(".news .news-item");
 
-	// nws_items.css("opacity","0");
-
-	// TweenMax.staggerFromTo( nws_items, 0.6,
-	// 	{
-	// 		opacity : 0
-	// 	},
-	// 	{
-	// 		opacity : 1
-	// 	}, 0.3 );
-}
-
+// Animating one page scroll pagination
 function animateLiveSection() {
-	// Animating one page scroll pagination
 	var live_tbs_items = $(".live_tabs .live_tab_button");
 
 	TweenMax.staggerFromTo( live_tbs_items, 1,
@@ -305,8 +603,9 @@ function animateLiveSection() {
 	}, 0.2 );
 }
 
+
+// Animating one page scroll pagination
 function animateContactsSection() {
-	// Animating one page scroll pagination
 	var cont_items = $(".phone_block");
 
 	TweenMax.staggerFromTo( cont_items, 1.5,
@@ -322,10 +621,13 @@ function animateContactsSection() {
 	}, 0.1 );
 }
 
+
+// Animating OnePageDots on right side
 function animateOnepageDots(state) {
 
 	var pg_items = $(".onepage-pagination").find("li");
 
+	// Show icons
 	if (state === 'show' && state != undefined) {
 
 		TweenMax.staggerFromTo( pg_items, 1,
@@ -342,6 +644,8 @@ function animateOnepageDots(state) {
 		}, 0.1 );
 
 	}
+
+	// Hide icons
 	else
 	{
 
