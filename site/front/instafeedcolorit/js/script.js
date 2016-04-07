dcc98103f7a7bb7b397eada6716252799ec21a1b
Dead Seagull <diode-bot@bk.ru>
2016-04-07 11:20:48 +0300

    Update script.js

--- site/front/instafeedcolorit/js/script.js
+++ site/front/instafeedcolorit/js/script.js
@@ -1,19 +1,5 @@
 if ( $("#instafeed").length )
 {
-    var instTemplate =
-    '<div class="col-lg-4 col-md-4">' +
-        '<div class="inst-item">'+
-            '<div class="inst-item__photo">'+
-                '<a href="{{link}}">'+
-                    '<img src="{{image}}" alt="">'+
-                '</a>'+
-            '</div>'+
-            '<div class="inst-item__meta">'+
-                '<span class="like"></span>'+
-                '<span>{{likes}}</span>'+
-            '</div>'+
-        '</div>' +
-    '</div>';
 
 	var template = _.template(
     '<div class="col-lg-4 col-md-4">' +
@@ -35,12 +21,11 @@ if ( $("#instafeed").length )
     var feed = new Instafeed({
         clientId: 'd84a51bb3706437991713e4f58f37cdc',
         accessToken: '2325935473.1677ed0.60e7653a64ff4f4db0d48fa5a8541425',
-        template: instTemplate,
         get: 'tagged',
-        tagName: 'cats',
+        tagName: 'selfie',
         limit: 6,
-		resolution: 'standard_resolution',
-		sortBy: 'most-recent',
+    		resolution: 'standard_resolution',
+    		sortBy: 'most-recent',
         mock: true,
 
         success: function(data) {
@@ -52,13 +37,12 @@ if ( $("#instafeed").length )
                 lastId = curId;
                 updatePhotos(data.data);
             }
-
-            console.log(isNoNews);
-            console.log(data.data);
         },
 
         after: function() {
-            $(".inst-item").animate({opacity: 1}, 200);
+            $("#instafeed").find(".inst-item").each(function() {
+                $(this).animate({opacity: 1}, 200);
+            });
         }
     });
 
@@ -80,11 +64,9 @@ if ( $("#instafeed").length )
 
         });
 
-        $("#instafeed").fadeOut(100, function() {
-            $(this).html(instaHTML).fadeIn(100);
-        });
+        var htmlContent = $("#instafeed").html();
+
+        $("#instafeed").html(instaHTML);
 
     }
-
-
 }
